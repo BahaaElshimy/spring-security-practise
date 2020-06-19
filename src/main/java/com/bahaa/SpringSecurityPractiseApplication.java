56b2aad9c6416216cf024863bb9f171a25574a1d
bahaa <bahaa.ahmed.baumy@gmail.com>
2020-06-20 02:39:28 +0300

    initialize project #14

---
+++ src/main/java/com/bahaa/SpringSecurityPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.bahaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractiseApplication.class, args);
+	}
+
+}
